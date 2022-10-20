be582df7ad24f9e8fa0c3e90ce890be3cad0c322
dovt@smartOSC <dovt@smartosc.com>
2022-10-20 18:03:31 +0700

    update Realm

--- RealmPlatform/RxRealm/Object+Rx.swift
+++ RealmPlatform/RxRealm/Object+Rx.swift
@@ -10,8 +10,15 @@ import RxSwift
 import RealmSwift
 
 extension Reactive where Base: Object {
-//    func sync<C: RealmRepresentableType, P>(entity: C,
-//                                           update: @escaping (P) -> Void) -> Observable<Result<P, Error>> where C.RealmType == P {
-//
-//    }
+    func first<R: Object>(ofType: R.Type = R.self, with predicate: (R) -> Bool) -> Observable<Result<R?, Error>> {
+        return Observable<Result<R?, Error>>.create { observer in
+            
+            return Disposables.create {}
+        }
+    }
+    
+    func sync<C: RealmRepresentableType, P>(entity: C,
+                                           update: @escaping (P) -> Void) -> Observable<Result<P, Error>> where C.RealmType == P {
+        
+    }
 }
